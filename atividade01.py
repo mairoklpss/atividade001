b3d72f40a6ba08ba1eb7cc61879aae3485e78d97
mairoklpss <mairalopesma@gmail.com>
2024-09-04 14:51:14 -0300

    soma de numero mais legal

--- atividade01.py
+++ atividade01.py
@@ -1,6 +1,6 @@
 # Crie um programa que receba dois números do usuário e exiba a soma desses números.
-num1 = int(input())
-num2 = int(input())
+num1 = int(input('digite um número: '))
+num2 = int(input('digite outro número: '))
 soma = num1 + num2
 
-print (soma)
+print (f'a soma dos dois números é {soma}')
